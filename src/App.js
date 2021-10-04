984d5c0ff9aae1e92886754cd4ed2cf2c3429c1d
yyogesh <yogeshggn1@gmail.com>
2021-10-04 08:26:01 +0530

    React-internationalize

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { FormattedMessage, } from 'react-intl';
+import { IntlProvider } from 'react-intl';
+
+
+const messages = {
+  en: {
+    greeting: "Hello {name}! How are you?",
+    time: "The time is {t, time, short}.",
+    date: "The date is {d, date}."
+  },
+  es: {
+    greeting: "¡Hola {name}! ¿Cómo estás?",
+    time: "La hora es {t, time, short}.",
+    date: "La fecha es {d, date}."
+  },
+  fr: {
+    greeting: "Bonjour {name}! Comment ça va?",
+    time: "Il est {t, time, short}.",
+    date: "La date est {d, date}."
+  },
+  de: {
+    greeting: "Hallo {name}! Wie geht's?",
+    time: "Es ist {t, time, short} Uhr.",
+    date: "Das Datum ist {d, date}."
+  }
+};
 
 function App() {
+  const [name, setName] = React.useState("");
+  const [locale, setLocale] = React.useState("en");
+  const handleChange = e => {
+    setName(e.target.value);
+  };
+  const handleSelect = e => {
+    setLocale(e.target.value);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <input placeholder="Enter name" onChange={handleChange} />
+      <select onChange={handleSelect} defaultValue={locale}>
+        {["en", "es", "fr", "de"].map(l => (
+          <option key={l}>{l}</option>
+        ))}
+      </select>
+      <div dangerouslySetInnerHTML={{ __html: '<h1>App</h1>' }} />
+      {/* <FormattedHTMLMessage
+        id="app.text"
+        defaultMessage="<h1>Hello, welcome {user} </h1>"
+        description="Welcome message"
+        values={{ user: "John Doe" }} /> */}
+
+      {/* <a href="https://www.google.com" target="_blank" rel="noopener noreferrer">
+        <FormattedMessage
+          id="app.link"
+          defaultMessage="Go to link"
+          description="Check website" />
+      </a> */}
+      <IntlProvider local={locale} messages={messages[locale]}>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <FormattedMessage id="greeting" values={{ name }} />
+          <br />
+          <FormattedMessage id="time" values={{ t: Date.now() }} />
+          <br />
+          <FormattedMessage id="date" values={{ d: Date.now() }} />
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </IntlProvider>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,10 +3,22 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { IntlProvider } from 'react-intl';
+import locale_en from "./translations/en.json";
+import locale_nl from "./translations/nl.json";
+
+const data = {
+  'nl': locale_nl,
+  'en': locale_en
+};
+
+const language = navigator.language.split(/[-_]/)[0];
 
 ReactDOM.render(
   <React.StrictMode>
+    {/* <IntlProvider local={language} messages={data[language]}> */}
     <App />
+    {/* </IntlProvider> */}
   </React.StrictMode>,
   document.getElementById('root')
 );
